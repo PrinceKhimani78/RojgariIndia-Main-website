44543eea771eb7c02a7ae59806458c2110db5dc6
Princekhimani78 <prince@mutanttechnologies.com>
2026-06-30 20:35:54 +0530

    fix: Resolve Vercel build error by removing invalid Navbar and Footer imports

--- src/app/recruiters/change-password/page.tsx
+++ src/app/recruiters/change-password/page.tsx
@@ -1,6 +1,3 @@
-import Navbar from "@/components/Common/Navbar";
-import React from "react";
-import Footer from "@/components/Common/Footer";
 import RecruiterChangePassword from "@/components/Recruiters/Change-password/Change-password";
 import type { Metadata } from "next";
 
@@ -8,14 +5,4 @@ export const metadata: Metadata = {
   title: "Change Password | Rojgari India",
 };
 
-const ChangePasswordPage = () => {
-  return (
-    <>
-      <Navbar />
-      <RecruiterChangePassword />
-      <Footer />
-    </>
-  );
-};
-
-export default ChangePasswordPage;
+export default RecruiterChangePassword;
